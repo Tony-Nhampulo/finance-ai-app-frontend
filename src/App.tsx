a86464193bc21cbd9d7c982aa805c20175c29119
Tony Nhampulo <geral@tonynhampulo.com>
2024-12-08 00:43:51 +0200

    Initial App setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="justify-center text-center flex items-center text-2xl font-bold text-red-500">
+        <p>Finance AI app</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
