ab686d140c347f1080c62a5f11d49b5f4fd993c6
Tony Nhampulo <geral@tonynhampulo.com>
2024-12-08 14:23:50 +0200

    Add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="text-primary flex items-center justify-center gap-2 text-center text-2xl font-bold">
-        <p>Finance AI app</p>
-        <ModeToggle />
-      </div>
+      <LoginPage />
     </ThemeProvider>
   );
 }
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+const LoginPage = () => {
+  return (
+    <div className="h-screen md:grid md:grid-cols-2">
+      {/* Left Side */}
+      <div className="mx-auto flex h-full max-w-[500px] flex-col justify-center p-8">
+        <img
+          src={"src/assets/logo.png"}
+          width={173}
+          height={39}
+          alt="Finance AI Logo"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-justify text-muted-foreground">
+          A Finance AI é uma plataforma de gestão fanceira que utiliza
+          Inteligência Artificial para monitorar suas finanças, e, oferecer
+          insights personalizados, facilitando o controle do seu orçamento.
+        </p>
+
+        <Button>
+          <LogInIcon />
+          Fazer Login ou Criar Conta
+        </Button>
+      </div>
+
+      {/* Right Side */}
+      <div
+        className="hidden h-full w-full bg-cover bg-center text-white md:block"
+        style={{ backgroundImage: `url(src/assets/login-page-background.png)` }}
+      ></div>
+    </div>
+  );
+};
+
+export default LoginPage;
